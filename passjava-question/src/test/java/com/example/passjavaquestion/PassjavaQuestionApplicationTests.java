f529d769b4704ce541849460fd495f5e77b93294
12051 <1205138308@qq.com>
2022-11-05 21:22:39 +0800

    NEW PROJECT

---
+++ passjava-question/src/test/java/com/example/passjavaquestion/PassjavaQuestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.passjavaquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaQuestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
